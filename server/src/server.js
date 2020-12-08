5f6848bf94cf84273c83f51777220cdc4a1ee001
Mahmoud Abuzeed <mahmoudabozaid56@gmail.com>
2020-12-08 09:12:13 +0200

    update server with category routes

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 // routes
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require("./routes/category");
 
 env.config();
 
@@ -43,3 +44,4 @@ app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
