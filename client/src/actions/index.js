28d37ece2852f30437b5010f5635ff151cb75766
Mahmoud Abuzeed <mahmoudabozaid56@gmail.com>
2020-12-08 19:18:19 +0200

    managed state of admin category

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,2 +1,3 @@
 export * from "./register.actions";
 export * from "./auth.actions";
+export * from "./category.actions";
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import adminRegisterReducer from "./admin/register.reducer";
 import AdminAuthReducer from "./admin/auth.reducer";
+import categoryReducer from "./admin/category.reducer";
 
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   admin: adminRegisterReducer,
   auth: AdminAuthReducer,
+  category: categoryReducer,
 });
 
 export default rootReducer;
